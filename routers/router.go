7d398a1a56fe4a041fbfb612b370bc993fc4389b
fukkong <rivirtu@gmail.com>
2018-12-07 22:40:55 +0900

    :fire: Remove useless code

--- routers/router.go
+++ routers/router.go
@@ -8,23 +8,11 @@
 package routers
 
 import (
-	"go-bee-restful/controllers"
-
 	"github.com/astaxie/beego"
 )
 
 func init() {
 	ns := beego.NewNamespace("/v1",
-		beego.NSNamespace("/object",
-			beego.NSInclude(
-				&controllers.ObjectController{},
-			),
-		),
-		beego.NSNamespace("/user",
-			beego.NSInclude(
-				&controllers.UserController{},
-			),
-		),
 	)
 	beego.AddNamespace(ns)
 }
