71a5bda69ca1346302eda165050cff39dd9ac209
fukkong <rivirtu@gmail.com>
2018-12-11 09:50:49 +0900

    Set server for cors problem

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/astaxie/beego/plugins/cors"
 	_ "go-bee-restful/routers"
 
 	"github.com/astaxie/beego"
@@ -10,6 +11,13 @@ func main() {
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+		beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+			AllowOrigins:     []string{"*"},
+			AllowMethods:     []string{"GET", "PUT", "POST", "DELETE"},
+			AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Content-Type"},
+			ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin"},
+			AllowCredentials: true,
+		}))
 	}
 	beego.Run()
 }
